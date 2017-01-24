a6841cc9f795b195b03dddfa4539f63267fae881
Simmyoungsub <rop2001@gmail.com>
2017-01-24 23:50:47 +0900

    package 계층 수정, BoardConstant 추가

---
+++ Board_2nd/src/main/java/com/spring/board/BoardConstant.java
@@ -0,0 +1,16 @@
+package com.spring.board;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BoardConstant {
+	@RequestMapping(value="/*.page", method=RequestMethod.GET)
+	public Model getPage(HttpServletRequest request, Model model){
+		return model;
+	}
+}
