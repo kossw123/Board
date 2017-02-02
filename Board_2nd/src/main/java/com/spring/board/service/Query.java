51069ac869f8b61e743921d1511499ce3163cce6
rop2001 <rop2001@gmail.com>
2017-02-02 16:04:20 +0900

    jdbctemplate 변경, 쿼리추가, 예외추가

--- Board_2nd/src/main/java/com/spring/board/service/Query.java
+++ Board_2nd/src/main/java/com/spring/board/service/Query.java
@@ -2,9 +2,9 @@ package com.spring.board.service;
 
 public enum Query {
 
-	GET_BOARD_LIST("select id, name from user.user","select"),
+	GET_BOARD_LIST("select seq, id, title, content, reg_date, mod_date  from user.board","select"),
 	UPDATE_BOARD_LIST("update set board where id=?","update"),
-	INSERT_BOARD_LIST("","insert"),
+	INSERT_BOARD_LIST("insert into user.board (id, title, content, reg_date, mod_date) values(:id,:title,:content,:reg_date,:mod_date)","insert"),
 	DELTE_BOARD_LIST("","delete");
 	
 	private String query;
---
+++ Board_2nd/src/main/java/com/spring/board/service/dao/BoardDao.java
@@ -0,0 +1,53 @@
+package com.spring.board.service.dao;
+
+import java.sql.Date;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BoardDao {
+	private int seq;
+	private String id;
+	private String title;
+	private String content;
+	private Date reg_date;
+	private Date mod_date;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getSeq() {
+		return seq;
+	}
+	public void setSeq(int seq) {
+		this.seq = seq;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public Date getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(Date reg_date) {
+		this.reg_date = reg_date;
+	}
+	public Date getMod_date() {
+		return mod_date;
+	}
+	public void setMod_date(Date mod_date) {
+		this.mod_date = mod_date;
+	}
+	
+}
