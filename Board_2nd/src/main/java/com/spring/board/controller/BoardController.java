f680415826be3b54c1665d0937ee4e345f11fb8e
rop2001 <rop2001@gmail.com>
2017-02-06 18:05:12 +0900

    예외추가중

--- Board_2nd/src/main/java/com/spring/board/controller/BoardController.java
+++ Board_2nd/src/main/java/com/spring/board/controller/BoardController.java
@@ -1,5 +1,6 @@
 package com.spring.board.controller;
 
+import java.security.InvalidParameterException;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.spring.board.exception.ApiCallException;
+import com.spring.board.exception.BoardException;
 import com.spring.board.service.ApiCallSerivce;
 import com.spring.board.service.Query;
 
@@ -55,10 +58,17 @@ public class BoardController {
 		Map<String,Object> resMap;
 		
 		try{
+			
+			if(!reqMap.containsKey("id") || !reqMap.containsKey("title") || !reqMap.containsKey("content")){
+				throw new InvalidParameterException();
+			}
+			
 			resMap = this.apiCallService.call(Query.INSERT_BOARD_LIST, reqMap);
 		}catch(SQLException e){
 			resMap = new HashMap<String,Object>();
 			resMap.put("message", e.getMessage());
+		}catch(InvalidParameterException e){
+			throw new BoardException("parameter","파라미터가 올바르지않습니다.");
 		}
 		
 		return resMap;
--- Board_2nd/src/main/java/com/spring/board/exception/BoardException.java
+++ Board_2nd/src/main/java/com/spring/board/exception/BoardException.java
@@ -1,8 +1,20 @@
 package com.spring.board.exception;
 
 public class BoardException extends RuntimeException {
+	private String code;
+	
 	public BoardException(){}
 	public BoardException(String message){
 		super(message);
+	}
+	
+	public BoardException(String code, String message){
+		this(code,message,null);
+	}
+	
+	public BoardException(String code , String message , Throwable cause){
+		super(message,cause);
+		
+		this.code = code;
 	}
 }
